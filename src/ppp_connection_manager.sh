cca880e92d490a85e9dcce0e6842912c4eb16a13
Robert Colvin <github@skrapost.net>
2022-10-20 10:00:13 +0200

    correct issue where script is always run once
    add capture of configure_modem.sh exit code and increment i if configure_modem.sh fails

--- src/ppp_connection_manager.sh
+++ src/ppp_connection_manager.sh
@@ -4,10 +4,12 @@ source functions.sh
 
 for i in {1..4}; do
     bash configure_modem.sh
+    MODEM_CONFIG=$?
     
     if [[ $MODEM_CONFIG -eq 0 ]]; then
         break
     fi
+    i=$(($i+1))
     sleep 1
 done
 
